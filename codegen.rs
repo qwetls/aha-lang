ae45669801072bafc797b73fb7cc8835789c1646
Often. <207790949+XeyyzuV2@users.noreply.github.com>
2025-10-29 22:59:10 -0700

    feat: add if/else conditional statements to AHA

---
+++ codegen.rs
@@ -0,0 +1,208 @@
+// src/codegen.rs
+
+use crate::ast;
+use inkwell::context::Context;
+use inkwell::module::Module;
+use inkwell::builder::Builder;
+use inkwell::values::{PointerValue, BasicValueEnum};
+use inkwell::types::IntType;
+use std::collections::HashMap;
+
+pub struct CodeGenerator<'ctx> {
+    context: &'ctx Context,
+    module: Module<'ctx>,
+    builder: Builder<'ctx>,
+    variables: HashMap<String, PointerValue<'ctx>>,
+    i64_type: IntType<'ctx>,
+}
+
+impl<'ctx> CodeGenerator<'ctx> {
+    pub fn new(context: &'ctx Context) -> Self {
+        let module = context.create_module("aha_module");
+        let builder = context.create_builder();
+        let i64_type = context.i64_type();
+
+        CodeGenerator {
+            context,
+            module,
+            builder,
+            variables: HashMap::new(),
+            i64_type,
+        }
+    }
+
+    pub fn compile(&mut self, program: &ast::Program) -> Result<(), String> {
+        let fn_type = self.i64_type.fn_type(&[], false);
+        let function = self.module.add_function("main", fn_type, None);
+        let basic_block = self.context.append_basic_block(function, "entry");
+
+        self.builder.position_at_end(basic_block);
+
+        for statement in &program.statements {
+            self.compile_statement(statement)?;
+        }
+
+        if let Some(last_stmt) = program.statements.last() {
+            if let ast::Statement::Expression(expr_stmt) = last_stmt {
+                let return_val = self.compile_expression(&expr_stmt.expression)?;
+                let _ = self.builder.build_return(Some(&return_val));
+                return Ok(());
+            }
+        }
+        
+        let zero = self.i64_type.const_int(0, false);
+        let _ = self.builder.build_return(Some(&zero));
+        
+        Ok(())
+    }
+
+    fn compile_statement(&mut self, statement: &ast::Statement) -> Result<(), String> {
+        match statement {
+            ast::Statement::Let(let_stmt) => {
+                let value = self.compile_expression(&let_stmt.value)?;
+                let pointer = self.builder.build_alloca(self.i64_type, &let_stmt.name.value)
+                    .map_err(|e| e.to_string())?;
+                self.builder.build_store(pointer, value)
+                    .map_err(|e| e.to_string())?;
+                self.variables.insert(let_stmt.name.value.clone(), pointer);
+            },
+            ast::Statement::Expression(expr_stmt) => {
+                self.compile_expression(&expr_stmt.expression)?;
+            },
+            ast::Statement::Return(_) => {
+                return Err("Return statement not yet implemented".to_string());
+            }
+        }
+        Ok(())
+    }
+
+    fn compile_expression(&mut self, expression: &ast::Expression) -> Result<BasicValueEnum<'ctx>, String> {
+        match expression {
+            ast::Expression::Integer(int_lit) => {
+                Ok(self.i64_type.const_int(int_lit.value as u64, false).into())
+            },
+            ast::Expression::Identifier(ident) => {
+                if let Some(pointer) = self.variables.get(&ident.value) {
+                    let loaded_val = self.builder.build_load(*pointer, &ident.value)
+                        .map_err(|e| e.to_string())?;
+                    Ok(loaded_val)
+                } else {
+                    Err(format!("Variable '{}' not found", ident.value))
+                }
+            },
+            ast::Expression::Infix(infix) => {
+                let left = self.compile_expression(&infix.left)?;
+                let right = self.compile_expression(&infix.right)?;
+                
+                match infix.operator.as_str() {
+                    "+" => Ok(self.builder.build_int_add(left.into_int_value(), right.into_int_value(), "addtmp")
+                        .map_err(|e| e.to_string())?.into()),
+                    "-" => Ok(self.builder.build_int_sub(left.into_int_value(), right.into_int_value(), "subtmp")
+                        .map_err(|e| e.to_string())?.into()),
+                    "*" => Ok(self.builder.build_int_mul(left.into_int_value(), right.into_int_value(), "multmp")
+                        .map_err(|e| e.to_string())?.into()),
+                    "/" => Ok(self.builder.build_int_signed_div(left.into_int_value(), right.into_int_value(), "divtmp")
+                        .map_err(|e| e.to_string())?.into()),
+                    "==" => {
+                        let cmp = self.builder.build_int_compare(inkwell::IntPredicate::EQ, left.into_int_value(), right.into_int_value(), "eqtmp")
+                            .map_err(|e| e.to_string())?;
+                        Ok(self.builder.build_int_z_extend(cmp, self.i64_type, "eqzext") // NAMA METHOD Y BENAR
+                            .map_err(|e| e.to_string())?.into())
+                    },
+                    "!=" => {
+                        let cmp = self.builder.build_int_compare(inkwell::IntPredicate::NE, left.into_int_value(), right.into_int_value(), "netmp")
+                            .map_err(|e| e.to_string())?;
+                        Ok(self.builder.build_int_z_extend(cmp, self.i64_type, "nezext") // NAMA METHOD Y BENAR
+                            .map_err(|e| e.to_string())?.into())
+                    },
+                    "<" => {
+                        let cmp = self.builder.build_int_compare(inkwell::IntPredicate::SLT, left.into_int_value(), right.into_int_value(), "lttmp")
+                            .map_err(|e| e.to_string())?;
+                        Ok(self.builder.build_int_z_extend(cmp, self.i64_type, "ltzext") // NAMA METHOD Y BENAR
+                            .map_err(|e| e.to_string())?.into())
+                    },
+                    ">" => {
+                        let cmp = self.builder.build_int_compare(inkwell::IntPredicate::SGT, left.into_int_value(), right.into_int_value(), "gttmp")
+                            .map_err(|e| e.to_string())?;
+                        Ok(self.builder.build_int_z_extend(cmp, self.i64_type, "gtzext") // NAMA METHOD Y BENAR
+                            .map_err(|e| e.to_string())?.into())
+                    },
+                    _ => Err(format!("Unknown operator: {}", infix.operator)),
+                }
+            },
+            ast::Expression::If(if_expr) => self.compile_if_expression(if_expr),
+            _ => Err("Expression type not yet implemented".to_string()),
+        }
+    }
+
+    fn compile_if_expression(&mut self, if_expr: &ast::IfExpression) -> Result<BasicValueEnum<'ctx>, String> {
+        let condition_val = self.compile_expression(&if_expr.condition)?;
+        
+        let function = self.builder.get_insert_block().expect("Error: Builder is not in a block!").get_parent().unwrap();
+        let consequence_block = self.context.append_basic_block(function, "consequence");
+        let alternative_block = self.context.append_basic_block(function, "alternative");
+        let merge_block = self.context.append_basic_block(function, "merge");
+
+        self.builder.build_conditional_branch(condition_val.into_int_value(), consequence_block, alternative_block)
+            .map_err(|e| e.to_string())?;
+
+        self.builder.position_at_end(consequence_block);
+        let consequence_val = self.compile_block_statement(&if_expr.consequence)?;
+        self.builder.build_unconditional_branch(merge_block)
+            .map_err(|e| e.to_string())?;
+
+        self.builder.position_at_end(alternative_block);
+        let alternative_val = if let Some(alt_block) = &if_expr.alternative {
+            self.compile_block_statement(alt_block)?
+        } else {
+            self.i64_type.const_int(0, false).into()
+        };
+        self.builder.build_unconditional_branch(merge_block)
+            .map_err(|e| e.to_string())?;
+
+        self.builder.position_at_end(merge_block);
+        let phi_node = self.builder.build_phi(self.i64_type, "iftmp")
+            .map_err(|e| e.to_string())?;
+        
+        phi_node.add_incoming(&[(&consequence_val, consequence_block), (&alternative_val, alternative_block)]);
+        
+        Ok(phi_node.as_basic_value())
+    }
+    
+    fn compile_block_statement(&mut self, block: &ast::BlockStatement) -> Result<BasicValueEnum<'ctx>, String> {
+        let mut last_value = self.i64_type.const_int(0, false).into();
+        for statement in &block.statements {
+            if let ast::Statement::Expression(expr_stmt) = statement {
+                last_value = self.compile_expression(&expr_stmt.expression)?;
+            } else {
+                self.compile_statement(statement)?;
+            }
+        }
+        Ok(last_value)
+    }
+
+    pub fn print_llvm_ir(&self) {
+        self.module.print_to_stderr();
+    }
+    
+    pub fn run_jit(&self) -> Result<i64, String> {
+        let execution_engine = self.module.create_jit_execution_engine(inkwell::OptimizationLevel::None)
+            .map_err(|e| format!("Failed to create JIT engine: {}", e))?;
+            
+        let function_name = "main";
+        let _function = self.module.get_function(function_name)
+            .ok_or_else(|| format!("Function '{}' not found", function_name))?;
+            
+        unsafe {
+            let compiled_fn: unsafe extern "C" fn() -> i64 = execution_engine.get_function_address(function_name)
+                .map_err(|e| format!("Failed to get function address: {}", e))
+                .map(|addr| std::mem::transmute(addr))?;
+                
+            Ok(compiled_fn())
+        }
+    }
+}
+
+
+
+
---
+++ parser.rs
@@ -0,0 +1,271 @@
+// src/parser.rs
+
+use crate::Lexer;
+use crate::ast;
+use crate::ast::{Program, Statement, Expression, Identifier, IntegerLiteral, BooleanLiteral, PrefixExpression, InfixExpression, LetStatement, ReturnStatement, ExpressionStatement, BlockStatement};
+use crate::ast::Token;
+use crate::ast::TokenType;
+
+pub struct Parser {
+    lexer: Lexer,
+    current_token: Token,
+    peek_token: Token,
+    pub errors: Vec<String>, // PASTIKAN INI PUBLIK
+}
+
+// Definisi presedensi operator
+#[derive(Debug, PartialEq, PartialOrd)]
+pub enum Precedence {
+    Lowest,
+    Equals,      // == or !=
+    LessGreater, // > or <
+    Sum,         // +
+    Product,     // *
+    Prefix,      // -X or !X
+    Call,        // myFunction(X)
+}
+
+impl Parser {
+    pub fn new(mut lexer: Lexer) -> Self {
+        let current_token = lexer.next_token();
+        let peek_token = lexer.next_token();
+
+        Parser {
+            lexer,
+            current_token,
+            peek_token,
+            errors: Vec::new(),
+        }
+    }
+
+    // Fungsi utama untuk mem-parse seluruh program
+    pub fn parse_program(&mut self) -> Program {
+        let mut program = Program { statements: Vec::new() };
+
+        while self.current_token.r#type != TokenType::Eof {
+            if let Some(stmt) = self.parse_statement() {
+                program.statements.push(stmt);
+            }
+            self.next_token();
+        }
+
+        program
+    }
+
+    // --- Helper Functions ---
+    fn next_token(&mut self) {
+        self.current_token = self.peek_token.clone();
+        self.peek_token = self.lexer.next_token();
+    }
+    
+    fn current_token_is(&self, t: TokenType) -> bool {
+        self.current_token.r#type == t
+    }
+
+    fn peek_token_is(&self, t: TokenType) -> bool {
+        self.peek_token.r#type == t
+    }
+
+    fn expect_peek(&mut self, t: TokenType) -> bool {
+        if self.peek_token_is(t) {
+            self.next_token();
+            true
+        } else {
+            self.peek_error(t);
+            false
+        }
+    }
+
+    // --- Parsing Functions ---
+    fn parse_statement(&mut self) -> Option<Statement> {
+        match self.current_token.r#type.clone() {
+            TokenType::Let => self.parse_let_statement(),
+            TokenType::Return => self.parse_return_statement(),
+            _ => self.parse_expression_statement(),
+        }
+    }
+
+    fn parse_let_statement(&mut self) -> Option<Statement> {
+        self.next_token(); // Lewati 'let'
+
+        if !self.current_token_is(TokenType::Identifier) {
+            self.errors.push(format!("expected next token to be Identifier, got {:?} instead", self.current_token.r#type));
+            return None;
+        }
+        let name = Identifier { value: self.current_token.literal.clone() };
+
+        if !self.expect_peek(TokenType::Assign) {
+            return None;
+        }
+
+        self.next_token(); // Lewati '='
+        let value = self.parse_expression(Precedence::Lowest);
+
+        if self.peek_token_is(TokenType::Semicolon) {
+            self.next_token(); // Lewati ';'
+        }
+
+        Some(Statement::Let(LetStatement { name, value }))
+    }
+
+    fn parse_return_statement(&mut self) -> Option<Statement> {
+        self.next_token(); // Lewati 'return'
+        let return_value = self.parse_expression(Precedence::Lowest);
+
+        if self.peek_token_is(TokenType::Semicolon) {
+            self.next_token(); // Lewati ';'
+        }
+
+        Some(Statement::Return(ReturnStatement { return_value }))
+    }
+
+    fn parse_expression_statement(&mut self) -> Option<Statement> {
+        let expression = self.parse_expression(Precedence::Lowest);
+        
+        if self.peek_token_is(TokenType::Semicolon) {
+            self.next_token();
+        }
+
+        Some(Statement::Expression(ExpressionStatement { expression }))
+    }
+
+    // --- Parsing Expressions (Versi Diperbaiki) ---
+    pub fn parse_expression(&mut self, precedence: Precedence) -> Expression {
+        let mut left = self.parse_prefix();
+
+        while !self.peek_token_is(TokenType::Semicolon) && precedence < self.peek_precedence() {
+            self.next_token(); // Ambil operator
+            let operator = self.current_token.literal.clone();
+            let right_precedence = self.current_precedence();
+            self.next_token(); // Pindah ke ekspresi di sebelah kanan
+            let right = Box::new(self.parse_expression(right_precedence));
+
+            left = Expression::Infix(InfixExpression {
+                left: Box::new(left),
+                operator,
+                right,
+            });
+        }
+
+        left
+    }
+    
+    fn parse_prefix(&mut self) -> Expression {
+        match self.current_token.r#type.clone() {
+            TokenType::Identifier => Expression::Identifier(Identifier { value: self.current_token.literal.clone() }),
+            TokenType::Integer => Expression::Integer(IntegerLiteral { value: self.current_token.literal.parse().unwrap() }),
+            TokenType::True => Expression::Boolean(BooleanLiteral { value: true }),
+            TokenType::False => Expression::Boolean(BooleanLiteral { value: false }),
+            TokenType::If => self.parse_if_expression(),
+            TokenType::Bang | TokenType::Minus => {
+                let operator = self.current_token.literal.clone();
+                self.next_token();
+                let right = Box::new(self.parse_expression(Precedence::Prefix));
+                Expression::Prefix(PrefixExpression { operator, right })
+            },
+            TokenType::LeftParen => {
+                self.next_token();
+                let exp = self.parse_expression(Precedence::Lowest);
+                if !self.expect_peek(TokenType::RightParen) {
+                    return Expression::Identifier(Identifier{ value: "ERROR".to_string() });
+                }
+                exp
+            }
+            _ => {
+                self.no_prefix_parse_fn_error(self.current_token.r#type.clone());
+                Expression::Identifier(Identifier{ value: "ERROR".to_string() })
+            }
+        }
+    }
+
+    // Fungsi baru untuk parsing if expression
+    fn parse_if_expression(&mut self) -> Expression {
+        self.next_token(); // Lewati 'if'
+
+        // Parse kondisi
+        let condition = self.parse_expression(Precedence::Lowest);
+
+        if !self.expect_peek(TokenType::LeftBrace) {
+            return Expression::Identifier(Identifier{ value: "ERROR".to_string() });
+        }
+
+        // Parse blok consequence
+        let consequence = self.parse_block_statement();
+
+        // Cek apakah ada 'else'
+        let alternative = if self.peek_token_is(TokenType::Else) {
+            self.next_token(); // Lewati 'else'
+            
+            // Cek apakah 'else' diikuti oleh 'if' (untuk if-else if)
+            if self.peek_token_is(TokenType::If) {
+                self.next_token(); // Lewati 'if'
+                // Rekursif untuk if-else if
+                Some(BlockStatement { statements: vec![Statement::Expression(ExpressionStatement{ expression: self.parse_if_expression() })] })
+            } else if self.expect_peek(TokenType::LeftBrace) {
+                // Parse blok else
+                Some(self.parse_block_statement())
+            } else {
+                return Expression::Identifier(Identifier{ value: "ERROR".to_string() });
+            }
+        } else {
+            None
+        };
+
+        Expression::If(ast::IfExpression {
+            condition: Box::new(condition),
+            consequence,
+            alternative,
+        })
+    }
+
+    // Fungsi baru untuk parsing blok statement { ... }
+    fn parse_block_statement(&mut self) -> BlockStatement {
+        self.next_token(); // Lewati '{'
+
+        let mut statements = Vec::new();
+
+        while !self.current_token_is(TokenType::RightBrace) && !self.current_token_is(TokenType::Eof) {
+            if let Some(stmt) = self.parse_statement() {
+                statements.push(stmt);
+            }
+            self.next_token();
+        }
+        
+        BlockStatement { statements }
+    }
+
+    // --- Presedence Helper ---
+    fn peek_precedence(&self) -> Precedence {
+        self.precedence(&self.peek_token.r#type)
+    }
+
+    fn current_precedence(&self) -> Precedence {
+        self.precedence(&self.current_token.r#type)
+    }
+
+    fn precedence(&self, t: &TokenType) -> Precedence {
+        match t {
+            TokenType::Eq => Precedence::Equals,
+            TokenType::NotEq => Precedence::Equals,
+            TokenType::LT => Precedence::LessGreater,
+            TokenType::GT => Precedence::LessGreater,
+            TokenType::Plus => Precedence::Sum,
+            TokenType::Minus => Precedence::Sum,
+            TokenType::Slash => Precedence::Product,
+            TokenType::Asterisk => Precedence::Product,
+            TokenType::LeftParen => Precedence::Call,
+            _ => Precedence::Lowest,
+        }
+    }
+    
+    // --- Error Handling ---
+    fn peek_error(&mut self, t: TokenType) {
+        let msg = format!("expected next token to be {:?}, got {:?} instead", t, self.peek_token.r#type);
+        self.errors.push(msg);
+    }
+    
+    fn no_prefix_parse_fn_error(&mut self, t: TokenType) {
+        let msg = format!("no prefix parse function for {:?} found", t);
+        self.errors.push(msg);
+    }
+}
